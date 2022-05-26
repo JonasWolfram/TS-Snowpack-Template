96086ffcbe932bbc74b55e13d7282abebd3601d5
Jonas Wolfram <jonas.wolfram@me.com>
2022-05-26 10:38:13 +0200

    007-Generics done - 008-Decorators started

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
 /* Your Code goes here */
+function Logger() {
+  return function (constructor: Function) {
+    console.log('Logging . . . ');
+    console.log(constructor);
+  };
+}
+
+@Logger()
+class Person {
+  name = 'Jonas';
+
+  constructor() {
+    console.log('Creating Person Object . . .');
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
